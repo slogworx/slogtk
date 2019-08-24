16e29a234f0fe5db2a3106ef050b265cc41c0c41
Slogworx Overlord <slog@slogworx.com>
2019-08-24 17:19:25 -0500

    added slogcrack and updated portscan

--- portscan.py
+++ portscan.py
@@ -4,19 +4,30 @@ import sys
 
 
 def pscan(host):
-    # Silence the verbosity
-    #conf.verb = False
-    packet = IP(dst=host)/TCP(flags="S", sport=randint(6001, 6999), dport=(20, 1024))
-    print('Probing ports 20 through 1024:\n')
-    ans, noa = sr(packet, timeout=2)
+    conf.verb = False  # Silence the verbosity
+    packet = IP(dst=host)/TCP(flags="S", sport=randint(6001, 6999), dport=(20, 1056))
+    answer, no_answer = sr(packet, timeout=10)
+    no_answer_ports, answer_ports = [], []
+    for noa in no_answer:
+        for n in noa:
+            no_answer_ports.append(n.dport)
 
-    for a in ans:
-        if not (a[1].sprintf("%TCP.flags%") == "RA"):
-            print(f"Service open on port {a[1].sport}")
+    for ans in answer:
+        for a in ans:
+            flag = a.getlayer(TCP).flags
+            if flag == 'SA':
+                answer_ports.append(a.sport)
+    return answer_ports, no_answer_ports
 
 
 if __name__ == "__main__":
     if not (len(sys.argv) == 2):
         print(f"usage: {sys.argv[0]} target_address")
     else:
-        pscan(sys.argv[1])
+        try:
+            answer, no_answer = pscan(sys.argv[1])
+        except OSError as e:
+            print(f'Invalid host: {e}')
+
+    print(f'closed ports: {no_answer}\n')
+    print(f'open ports: {answer}\n')
---
+++ slogcrack.py
@@ -0,0 +1,26 @@
+import hashlib
+import time
+import sys
+
+#TEST_HASH_TO_CRACK = '3a3a5c3f10e14cc9d5e92127a0ee0880'  # password == vjht008
+#TEST_HASH_TO_CRACK = '54d381f9733bede2d0f532654ad9f7d2'  # password == Unicorn69
+
+
+def crack_hash(pw_list, hashed_pw):
+    start_time = time.time()
+    cracked_pw = "Not in list"
+    with open(pw_list, "r") as pl:
+        for pw in pl:
+            hash = hashlib.md5(pw.strip().encode('utf8')).hexdigest()
+            if hash == hashed_pw:
+                cracked_pw = pw
+                break
+    end_time = time.time()
+    return f'HASH[ {hashed_pw} ] == "{cracked_pw.strip()}". It took {end_time - start_time} seconds.'
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print('usage: {sys.argv[0]} password_file MD5_hash')
+    else:
+        print(crack_hash(sys.argv[1], sys.argv[2]))
